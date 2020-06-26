a53261d28cb222e73d797787f9d1b5dabf79b448
Dhanasekaran Subramani <dhanasekaran.subramani@rabobank.nl>
2020-06-26 15:29:52 +0530

    JUnit test for methods writing to OutputStream - JUnit test for System.out.println()

---
+++ src/test/java/com/ds2/junit/logging/SetOutTest.java
@@ -0,0 +1,45 @@
+package com.ds2.junit.logging;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class SetOutTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+
+    @Test
+    public void out() {
+        System.out.print("hello");
+        assertEquals("hello", outContent.toString());
+    }
+
+    @Test
+    public void err() {
+        System.err.print("hello again");
+        assertEquals("hello again", errContent.toString());
+    }
+}
+
---
+++ src/test/java/com/ds2/junit/logging/SystemOutTest.java
@@ -0,0 +1,32 @@
+package com.ds2.junit.logging;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class SystemOutTest {
+
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(output));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
+    @Test
+    public void testSystemOut() {
+        System.out.print("assertme!");
+        assertEquals("assertme!", output.toString());
+    }
+
+}
