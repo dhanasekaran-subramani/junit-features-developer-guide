e9cdb82573c391014b9c84e7cecd0a474c53e35b
Dhanasekaran Subramani <dhanasekaran.subramani@rabobank.nl>
2020-06-26 19:17:43 +0530

    JUnit Ignore Test Example

---
+++ src/test/java/com/ds2/junit/ignoring/TestCaseIgnoreTest.java
@@ -0,0 +1,38 @@
+package com.ds2.junit.ignoring;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TestCaseIgnoreTest {
+
+    @Test
+    public void test1() {
+        assertThat(1 + 1, is(2));
+    }
+
+    @Ignore
+    @Test
+    public void test2() {
+        assertThat(1 + 2, is(3));
+    }
+
+    @Test
+    public void test3() {
+        assertThat(1 + 3, is(4));
+    }
+
+    @Ignore("Test is ignored as a demonstration")
+    @Test
+    public void test4() {
+        assertThat(1 + 4, is(5));
+    }
+
+    @Test
+    public void test5() {
+        assertThat(1 + 5, is(6));
+    }
+
+}
